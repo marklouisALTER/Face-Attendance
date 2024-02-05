723f2e0a59ca8ad8685c50e78d15f0da1cbd3da8
Mark Louis Bernardo <marklouisbernando318@gmail.com>
2024-02-05 11:53:13 +0800

    make the host public and default in port

--- backend/app.py
+++ backend/app.py
@@ -25,4 +25,4 @@ app.register_blueprint(face_recognition_bp, url_prefix='/face_recognition')
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=5000)
